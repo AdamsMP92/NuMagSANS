a96026463d88c6558a29bd331e5692cdda4df406
AdamsMP <87016586+AdamsMP92@users.noreply.github.com>
2025-10-28 11:43:09 +0100

    Update NuMagSANSlib_SpectralData.h

--- src/NuMagSANSlib_SpectralData.h
+++ src/NuMagSANSlib_SpectralData.h
@@ -30,25 +30,27 @@
 using namespace std;
 
 struct SpectralData {
-    unsigned int *Nq;        // Anzahl q-Werte
-    unsigned int *Ntheta;    // Anzahl theta-Werte
-    unsigned int *k_max;     // maximaler k-Wert
-    float *dtheta; // Winkel-Schrittweite
+   unsigned int *Nq;        // number of q-values
+   unsigned int *Ntheta;    // number of theta-values
+   unsigned int *k_max;     // maximum number of modes
+   float *dtheta;           // angular step-size
 
-    // the arrays are single column and contain the angular sine and cosine spectra
-    // the total array length is 2 * Nq * k_max
-    float *I_Nuc_2D_unpolarized;		  // nuclear SANS cross section
-  	float *I_Mag_2D_unpolarized;		  // unpolarized magnetic SANS cross section
-  	float *I_Mag_2D_polarized;			  // polarized magnetic SANS cross section
-  	float *I_NucMag_2D;					      // nuclear-magnetic interference SANS cross section
-  	float *I_Mag_2D_spin_flip;			  // spin-flip magnetic SANS cross section
-  	float *I_Mag_2D_chiral;				    // chiral magnetic SANS cross section
-  	float *I_Mag_2D_spin_flip_pm;		  // pm-spin-flip magnetic SANS cross section
-  	float *I_Mag_2D_spin_flip_mp;		  // mp-spin-flip magnetic SANS cross section
-  	float *I_Mag_2D_non_spin_flip_pp;	// pp-non-spin-flip magnetic SANS cross section
-  	float *I_Mag_2D_non_spin_flip_mm;	// mm-non-spin-flip magnetic SANS cross section
-  	float *I_Mag_2D_sanspol_p;			  // p-sanspol magnetic SANS cross section
-  	float *I_Mag_2D_sanspol_m;			  // m-sanspol magnetic SANS cross section
+   float *q;
+
+   // the arrays are single column and contain the angular sine and cosine spectra
+   // the total array length is 2 * Nq * k_max
+   float *I_Nuc_unpolarized;		  // nuclear SANS cross section
+  	float *I_Mag_unpolarized;		  // unpolarized magnetic SANS cross section
+  	float *I_Mag_polarized;			  // polarized magnetic SANS cross section
+  	float *I_NucMag;					      // nuclear-magnetic interference SANS cross section
+  	float *I_Mag_spin_flip;			  // spin-flip magnetic SANS cross section
+  	float *I_Mag_chiral;				    // chiral magnetic SANS cross section
+  	float *I_Mag_spin_flip_pm;		  // pm-spin-flip magnetic SANS cross section
+  	float *I_Mag_spin_flip_mp;		  // mp-spin-flip magnetic SANS cross section
+  	float *I_Mag_non_spin_flip_pp;	// pp-non-spin-flip magnetic SANS cross section
+  	float *I_Mag_non_spin_flip_mm;	// mm-non-spin-flip magnetic SANS cross section
+  	float *I_Mag_sanspol_p;			  // p-sanspol magnetic SANS cross section
+  	float *I_Mag_sanspol_m;			  // m-sanspol magnetic SANS cross section
 
 };
 
@@ -67,35 +69,39 @@ void allocate_SpectralData_RAM(InputFileData* InputData, \
     *SpecData->k_max = InputData->k_max;
     *SpecData->dtheta = *SANSData->dtheta;
 
-    // --- Allocate spectral arrays ---
-    size_t len = 2 * (*(SpecData->Nq)) * (*(SpecData->k_max)) * sizeof(float);
+    // allocate the q-space vector
+    SpecData->q      = (float*) malloc((*(SpecData->Nq))*sizeof(float));
+    memset(SpecData->q, 0, (*SpecData->Nq) * sizeof(float));
+    memcpy(SpecData->q, SANSData->q_1D, (*SpecData->Nq) * sizeof(float));
 
-    SpecData->I_Nuc_2D_unpolarized      = (float*) malloc(len);
-    SpecData->I_Mag_2D_unpolarized      = (float*) malloc(len);
-    SpecData->I_Mag_2D_polarized        = (float*) malloc(len);
-    SpecData->I_NucMag_2D               = (float*) malloc(len);
-    SpecData->I_Mag_2D_spin_flip        = (float*) malloc(len);
-    SpecData->I_Mag_2D_chiral           = (float*) malloc(len);
-    SpecData->I_Mag_2D_spin_flip_pm     = (float*) malloc(len);
-    SpecData->I_Mag_2D_spin_flip_mp     = (float*) malloc(len);
-    SpecData->I_Mag_2D_non_spin_flip_pp = (float*) malloc(len);
-    SpecData->I_Mag_2D_non_spin_flip_mm = (float*) malloc(len);
-    SpecData->I_Mag_2D_sanspol_p        = (float*) malloc(len);
-    SpecData->I_Mag_2D_sanspol_m        = (float*) malloc(len);
+    // allocate spectral arrays
+    size_t len = 2 * (*(SpecData->Nq)) * (*(SpecData->k_max)) * sizeof(float);
+    SpecData->I_Nuc_unpolarized      = (float*) malloc(len);
+    SpecData->I_Mag_unpolarized      = (float*) malloc(len);
+    SpecData->I_Mag_polarized        = (float*) malloc(len);
+    SpecData->I_NucMag               = (float*) malloc(len);
+    SpecData->I_Mag_spin_flip        = (float*) malloc(len);
+    SpecData->I_Mag_chiral           = (float*) malloc(len);
+    SpecData->I_Mag_spin_flip_pm     = (float*) malloc(len);
+    SpecData->I_Mag_spin_flip_mp     = (float*) malloc(len);
+    SpecData->I_Mag_non_spin_flip_pp = (float*) malloc(len);
+    SpecData->I_Mag_non_spin_flip_mm = (float*) malloc(len);
+    SpecData->I_Mag_sanspol_p        = (float*) malloc(len);
+    SpecData->I_Mag_sanspol_m        = (float*) malloc(len);
 
     // --- Initialize to zero ---
-    memset(SpecData->I_Nuc_2D_unpolarized, 0, len);
-    memset(SpecData->I_Mag_2D_unpolarized, 0, len);
-    memset(SpecData->I_Mag_2D_polarized, 0, len);
-    memset(SpecData->I_NucMag_2D, 0, len);
-    memset(SpecData->I_Mag_2D_spin_flip, 0, len);
-    memset(SpecData->I_Mag_2D_chiral, 0, len);
-    memset(SpecData->I_Mag_2D_spin_flip_pm, 0, len);
-    memset(SpecData->I_Mag_2D_spin_flip_mp, 0, len);
-    memset(SpecData->I_Mag_2D_non_spin_flip_pp, 0, len);
-    memset(SpecData->I_Mag_2D_non_spin_flip_mm, 0, len);
-    memset(SpecData->I_Mag_2D_sanspol_p, 0, len);
-    memset(SpecData->I_Mag_2D_sanspol_m, 0, len);
+    memset(SpecData->I_Nuc_unpolarized, 0, len);
+    memset(SpecData->I_Mag_unpolarized, 0, len);
+    memset(SpecData->I_Mag_polarized, 0, len);
+    memset(SpecData->I_NucMag, 0, len);
+    memset(SpecData->I_Mag_spin_flip, 0, len);
+    memset(SpecData->I_Mag_chiral, 0, len);
+    memset(SpecData->I_Mag_spin_flip_pm, 0, len);
+    memset(SpecData->I_Mag_spin_flip_mp, 0, len);
+    memset(SpecData->I_Mag_non_spin_flip_pp, 0, len);
+    memset(SpecData->I_Mag_non_spin_flip_mm, 0, len);
+    memset(SpecData->I_Mag_sanspol_p, 0, len);
+    memset(SpecData->I_Mag_sanspol_m, 0, len);
  
 }
 
@@ -115,9 +121,28 @@ void init_SpectralData(InputFileData *InputData, \
 
 }
 
-void free_SpectralData(AngularSpectrum* A)
+void free_SpectralData(SpectralData* S, \
+                       SpectralData* S_gpu)
 {
-    
+
+    free(S->Nq);
+    free(S->Ntheta);
+    free(S->k_max);
+    free(S->dtheta);
+    free(S->q);
+    free(S->I_Nuc_unpolarized);
+    free(S->I_Mag_unpolarized);
+    free(S->I_Mag_polarized);
+    free(S->I_NucMag);
+    free(S->I_Mag_spin_flip);
+    free(S->I_Mag_chiral);
+    free(S->I_Mag_spin_flip_pm);
+    free(S->I_Mag_spin_flip_mp);
+    free(S->I_Mag_non_spin_flip_pp);
+    free(S->I_Mag_non_spin_flip_mm);
+    free(S->I_Mag_sanspol_p);
+    free(S->I_Mag_sanspol_m);
+ 
 }
 
 
