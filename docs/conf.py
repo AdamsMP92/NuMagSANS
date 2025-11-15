48ba2aead398fc5a19d8a04a7f615bd5662bb202
AdamsMP <87016586+AdamsMP92@users.noreply.github.com>
2025-11-16 00:16:26 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]   # ← WICHTIG!
 
-#html_logo = "_static/logo.png"
-#html_favicon = "_static/favicon.png"
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.png"
 
 html_show_sourcelink = False
 html_sourcelink_suffix = ""
