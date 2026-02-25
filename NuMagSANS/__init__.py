28858b49bee026ee8bf9e0a4e6f082f01914eec1
AdamsMP <87016586+AdamsMP92@users.noreply.github.com>
2026-02-25 15:51:38 +0100

    Import NuMagSANS from the correct module

--- NuMagSANS/__init__.py
+++ NuMagSANS/__init__.py
@@ -5,6 +5,6 @@ Provides a high-level Python facade for executing the
 NuMagSANS CUDA backend.
 """
 
-from .NuMagSANSFacade import NuMagSANS
+from .NuMagSANS import NuMagSANS
 
 __all__ = ["NuMagSANS"]
