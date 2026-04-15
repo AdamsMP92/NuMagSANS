c338d53ecce69186083c5253f40820d68d0dfdb4
AdamsMP <87016586+AdamsMP92@users.noreply.github.com>
2026-04-15 15:17:11 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ html_sourcelink_suffix = ""
 
 html_theme_options = {
     "external_links": [
+        {"name": "Home", "url": "https://adamsmp92.github.io/NuMagSANS/index.html"},
         {"name": "GitHub Repository", "url": "https://github.com/AdamsMP92/NuMagSANS"},
     ],
     "icon_links": [
